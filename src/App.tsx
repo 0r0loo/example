b68c79a24e824594e0c1f1ca6984c853fea5ddb6
chotaegyu <chotg5592@gmail.com>
2025-06-24 22:19:53 +0900

    FEAT: prettier 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-
 function App() {
-
-  return (
-    <div className={'bg-teal-400 text-pink-500'}>
-        hello world
-    </div>
-  )
+  return <div className={'bg-teal-400 text-pink-500'}>hello world</div>;
 }
 
-export default App
+export default App;
